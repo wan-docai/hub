254ce0708ed6fdfc398103b44abb8baed2e0a8ec
Wan Zhu <wan@Wans-MacBook-Pro.local>
2019-03-29 11:03:20 -0700

    revise retrain_dev

--- examples/image_retraining/retrain_dev.py
+++ examples/image_retraining/retrain_dev.py
@@ -929,12 +929,16 @@ def add_jpeg_decoding(module_spec):
     Tensors for the node to feed JPEG data into, and the output of the
       preprocessing steps.
   """
+
+  input_height, input_width = hub.get_expected_image_size(module_spec)
+  input_depth = hub.get_num_image_channels(module_spec)
+  jpeg_data = tf.placeholder(tf.string, name='DecodeJPGInput')
   try:
-      input_height, input_width = hub.get_expected_image_size(module_spec)
-      input_depth = hub.get_num_image_channels(module_spec)
-      jpeg_data = tf.placeholder(tf.string, name='DecodeJPGInput')
       decoded_image = tf.image.decode_jpeg(jpeg_data, channels=input_depth)
       # Convert from full range of uint8 to range [0,1] of float32.
+  except Exception as e:
+      print("Failed to add to archive: {0}".format(str(e)))
+      continue
       decoded_image_as_float = tf.image.convert_image_dtype(decoded_image,
                                                             tf.float32)
       decoded_image_4d = tf.expand_dims(decoded_image_as_float, 0)
@@ -942,8 +946,7 @@ def add_jpeg_decoding(module_spec):
       resize_shape_as_int = tf.cast(resize_shape, dtype=tf.int32)
       resized_image = tf.image.resize_bilinear(decoded_image_4d,
                                                resize_shape_as_int)
-  except Exception as e:
-      print("Failed to add to archive: {0}".format(str(e)))
+
   return jpeg_data, resized_image
 
 
